5aced5a5e26074d4b3f0a14fb03b7f73767c0a52
Nguyen Van Phuc <nvpdldolympia24@gmail.com>
2024-07-13 02:01:52 +0700

    BK Robotics

---
+++ MDK-ARM/mini_project/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+mini_project\startup_stm32f103xb.o: startup_stm32f103xb.s
---
+++ MDK-ARM/mini_project/stm32f1xx_hal_msp.d
@@ -0,0 +1,31 @@
+mini_project\stm32f1xx_hal_msp.o: ../Core/Src/stm32f1xx_hal_msp.c
+mini_project\stm32f1xx_hal_msp.o: ../Core/Inc/main.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+mini_project\stm32f1xx_hal_msp.o: ../Core/Inc/stm32f1xx_hal_conf.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Include/core_cm3.h
+mini_project\stm32f1xx_hal_msp.o: D:\Keli v5\ARM\ARMCC\Bin\..\include\stdint.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Include/cmsis_version.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+mini_project\stm32f1xx_hal_msp.o: D:\Keli v5\ARM\ARMCC\Bin\..\include\stddef.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h
+mini_project\stm32f1xx_hal_msp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
